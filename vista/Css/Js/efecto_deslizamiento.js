45fac10d1ae653d4e370708675d997f34620cd8c
LynkRookie <lynkrookie@gmail.com>
2024-09-10 02:40:09 -0300

    m

--- vista/Css/Js/efecto_deslizamiento.js
+++ vista/Css/Js/efecto_deslizamiento.js
@@ -1,7 +1,7 @@
 document.addEventListener('wheel', function(event) {
         // Manejo de agrandamiento de contenedores al hacer scroll
         const contenedor = document.querySelector('.contenedor');
-        const contenedor2 = document.querySelector('.contenedor_2');
+        
         if (event.deltaY > 0) {
             // Si el desplazamiento es hacia abajo, agranda ambos contenedores
             if (contenedor) contenedor.classList.add('scroll-aumentado');
@@ -13,13 +13,18 @@ document.addEventListener('wheel', function(event) {
                 debueltaMauseElements.forEach(element => element.classList.remove('scroll-aumentado'));
             }, 500); // Ajusta el tiempo si es necesario
         }
-        if(event.deltaY > 0){
-             if (contenedor2) contenedor2.classList.add('scroll-aumentado');
-        }else{
-             if (contenedor2) contenedor2.classList.remove('scroll-aumentado'); 
-        }
+        
     }
 );
+    document.addEventListener('wheel', function(event) {
+            const contenedor2 = document.querySelector('.contenedor_2');
+            if(event.deltaY > 0){
+                if (contenedor2) contenedor2.classList.add('scroll-aumentado');
+            }else{
+                if (contenedor2) contenedor2.classList.remove('scroll-aumentado'); 
+            }
+        }
+    );
   // Manejo de agrandamiento de imágenes en certificados al pasar el mouse
   document.addEventListener('mouseover', function(event) {
     if (event.target.closest('.curso1')) {
