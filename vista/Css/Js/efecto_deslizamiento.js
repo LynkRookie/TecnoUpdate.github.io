716c12195909e8fa9617825d38e741ae7d716342
LynkRookie <lynkrookie@gmail.com>
2024-09-10 02:03:31 -0300

    a

--- vista/Css/Js/efecto_deslizamiento.js
+++ vista/Css/Js/efecto_deslizamiento.js
@@ -1,7 +1,8 @@
 document.addEventListener('wheel', function(event) {
+    // Manejo de agrandamiento de contenedores al hacer scroll
     const contenedor = document.querySelector('.contenedor');
     const contenedor2 = document.querySelector('.contenedor_2');
-  
+    
     if (event.deltaY > 0) {
       // Si el desplazamiento es hacia abajo, agranda ambos contenedores
       if (contenedor) contenedor.classList.add('scroll-aumentado');
@@ -13,4 +14,16 @@ document.addEventListener('wheel', function(event) {
     }
   });
   
+  // Manejo de agrandamiento de imágenes en certificados al pasar el mouse
+  document.addEventListener('mouseover', function(event) {
+    if (event.target.closest('.curso1')) {
+      event.target.closest('.curso1').classList.add('hover-aumentado');
+    }
+  });
+  
+  document.addEventListener('mouseout', function(event) {
+    if (event.target.closest('.curso1')) {
+      event.target.closest('.curso1').classList.remove('hover-aumentado');
+    }
+  });
   
