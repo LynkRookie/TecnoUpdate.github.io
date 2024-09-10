b66e2cea79f9da28b54244b350cf61713fcb498d
LynkRookie <lynkrookie@gmail.com>
2024-09-10 01:50:04 -0300

    a

--- vista/Css/Js/efecto_deslizamiento.js
+++ vista/Css/Js/efecto_deslizamiento.js
@@ -1,11 +1,16 @@
 document.addEventListener('wheel', function(event) {
     const contenedor = document.querySelector('.contenedor');
+    const contenedor2 = document.querySelector('.contenedor_2');
+  
     if (event.deltaY > 0) {
-      // Si el desplazamiento es hacia abajo, agranda el contenedor
-      contenedor.classList.add('scroll-aumentado');
+      // Si el desplazamiento es hacia abajo, agranda ambos contenedores
+      if (contenedor) contenedor.classList.add('scroll-aumentado');
+      if (contenedor2) contenedor2.classList.add('scroll-aumentado');
     } else {
       // Si el desplazamiento es hacia arriba, vuelve al tamaño original
-      contenedor.classList.remove('scroll-aumentado');
+      if (contenedor) contenedor.classList.remove('scroll-aumentado');
+      if (contenedor2) contenedor2.classList.remove('scroll-aumentado');
     }
   });
+  
   
